ef086e9faaf4a0e9c29434e80ca261875aad5d38
Todd Seller <me@toddseller.com>
2016-01-24 14:58:39 -0800

    Commit to pull update code

--- sudoku.rb
+++ sudoku.rb
@@ -4,6 +4,15 @@
 # your solver has tried to solve it.
 # How you represent your board is up to you!
 def solve(board_string)
+  return board if solved?
+
+  board = populate_board(board_string)
+  board.each do |id|
+
+   p board[3]
+  end
+
+  return board
 end
 
 # Returns a boolean indicating whether
@@ -19,4 +28,26 @@ end
 # The input board will be in whatever
 # form `solve` returns.
 def pretty_board(board)
+end
+def populate_board(board_string)
+  return [
+    {id:0, row: 0, column: 0, box: 0, value: '5'},
+    {id:1, row: 0, column: 1, box: 0, value: ['1','2','3','4','5','6','7','8','9']},
+    {id:2, row: 0, column: 2, box: 0, value: '2'},
+    {id:3, row: 0, column: 3, box: 1, value: '3'},
+    {id:4, row: 0, column: 4, box: 1, value: '4'},
+    {id:5, row: 0, column: 5, box: 1, value: ['1','2','3','4','5','6','7','8','9']},
+    {id:6, row: 0, column: 6, box: 2, value: '7'},
+    {id:7, row: 0, column: 7, box: 2, value: '8'},
+    {id:8, row: 0, column: 8, box: 2, value: ['1','2','3','4','5','6','7','8','9']},
+    {id:9, row: 0, column: 0, box: 0, value: '7'},
+    {id:10, row: 1, column: 1, box: 0, value: ['1','2','3','4','5','6','7','8','9']},
+    {id:11, row: 1, column: 2, box: 0, value: ['1','2','3','4','5','6','7','8','9']},
+    {id:12, row: 1, column: 3, box: 1, value: ['1','2','3','4','5','6','7','8','9']},
+    {id:13, row: 1, column: 4, box: 1, value: ['1','2','3','4','5','6','7','8','9']},
+    {id:14, row: 1, column: 5, box: 1, value: ['1','2','3','4','5','6','7','8','9']},
+    {id:15, row: 1, column: 6, box: 2, value: ['1','2','3','4','5','6','7','8','9']},
+    {id:16, row: 1, column: 7, box: 2, value: ['1','2','3','4','5','6','7','8','9']},
+    {id:17, row: 1, column: 8, box: 2, value: ['1','2','3','4','5','6','7','8','9']},
+    ]
 end
